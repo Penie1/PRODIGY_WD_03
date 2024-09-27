b2a00a27c3a851113eb4297ce35633e0b2d9593d
Peniel Bekele <142409326+Penie1@users.noreply.github.com>
2024-09-27 11:35:13 +0300

    Create a gameboard

--- javascript/app.js
+++ javascript/app.js
@@ -1,1 +1,18 @@
 "use strict";
+
+function Gameboard() {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(0);
+    }
+  }
+
+  const getBoard = () => board;
+
+  return { getBoard };
+}
